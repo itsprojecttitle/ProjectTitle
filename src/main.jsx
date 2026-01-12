e0e0bfc76fff2ee025543fd13e3d0e965b3a9b61
Samuel Apata <samuelapata@Samuels-MBP.Home>
2026-01-12 16:50:50 +0000

    Revert "Revert "Lock body scroll for iOS safe area behavior""
    This reverts commit e9dd4ceea5fd76bfa1759bf3e820468f1ed26a04.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/tailwind-runtime.css";
 import "./styles/index.css";
+import applyBodyLock from "./utils/applyBodyLock.js";
+
+applyBodyLock();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
--- src/service-packages.jsx
+++ src/service-packages.jsx
@@ -3,5 +3,8 @@ import { createRoot } from "react-dom/client";
 import ServicePackagesApp from "./ServicePackagesApp.jsx";
 import "./styles/tailwind-runtime.css";
 import "./styles/index.css";
+import applyBodyLock from "./utils/applyBodyLock.js";
+
+applyBodyLock();
 
 createRoot(document.getElementById("root")).render(<ServicePackagesApp />);

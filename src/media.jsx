a3f46b8789e017e5c32addb7d1eecf8d078f4b55
Samuel Apata <samuelapata@Samuels-MacBook-Pro.local>
2026-01-12 16:13:42 +0000

    Revert "Restore native scrolling"
    This reverts commit 7efb0454802f03d469355f82d3a7348c8db831dc.

--- src/media.jsx
+++ src/media.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import MediaApp from "./MediaApp.jsx";
 import "./styles/tailwind-runtime.css";
 import "./styles/index.css";
+import applyBodyLock from "./utils/applyBodyLock.js";
+
+applyBodyLock();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>

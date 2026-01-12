10f7b72fb85af413b5c09ba5201ccd028b8d5175
Samuel Apata <samuelapata@Samuels-MBP.Home>
2026-01-12 16:50:50 +0000

    Revert "Lock body scroll for iOS safe area behavior"
    This reverts commit 527d0c0ef2c5a95b59a43f52633c7b2861a38b28.

--- src/full-portfolio.jsx
+++ src/full-portfolio.jsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import FullPortfolioApp from "./FullPortfolioApp.jsx";
 import "./styles/tailwind-runtime.css";
 import "./styles/index.css";
-import applyBodyLock from "./utils/applyBodyLock.js";
-
-applyBodyLock();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>

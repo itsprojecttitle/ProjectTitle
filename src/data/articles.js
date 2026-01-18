4cc113f827d1414a282528f6a08ec4524bd70936
ProjectTitle <itsprojecttitle@gmail.com>
2026-01-18 14:25:00 +0000

    Update news image asset

--- src/data/articles.js
+++ src/data/articles.js
@@ -3,7 +3,7 @@ export const articleItems = [
         box: 1,
         title: "Upcoming Events",
         date: "Jan, 3, 2026",
-        image: "/assets/news/essdekid-templates-recovered.png",
+        image: "/assets/news/kas d.png",
         link: "https://www.instagram.com/p/DTpuIXhgh0d/?img_index=1",
     },
 ];
